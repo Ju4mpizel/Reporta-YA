298a65a29d052160be5272ecb52bb9b591b5534d
Ju4mpizel <juampizel@gmail.com>
2026-09-08 12:52:43 -0400

    chore: eliminar archivos de prueba

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -3,10 +3,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import "react-native-url-polyfill/auto";
 
-const SUPABASE_URL =
-  process.env.EXPO_PUBLIC_SUPABASE_URL || "https://TU_PROYECTO.supabase.co";
-const SUPABASE_ANON_KEY =
-  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "TU_ANON_KEY";
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
